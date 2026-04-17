ad9bc92cd223426deefa8390d2ec05bc549bd576
aditya waykole <adityawaykole494@gmail.com>
2026-04-17 21:18:00 +0530

    object start

---
+++ day_8object/index.js
@@ -0,0 +1,44 @@
+//object
+
+//key value
+
+const user = {
+    nam:"aditya",
+    age:20,
+    amt:5400
+};
+
+console.log(user)
+
+//CRUD operation : create read update delete 
+
+
+//update 
+// user.adhar = 123;
+// user.amount = 463547;
+
+// // console.log(user)
+
+// //delete
+// delete user.adhar;
+// console.log(user);
+
+// //read
+// console.log(user["amount"])
+
+
+//you want a seperate kesy , values 
+
+console.log(user)
+
+
+console.log(Object.keys(user));
+
+console.log(Object.values(user));
+
+console.log(Object.entries(user));
+
+
+
+
+
