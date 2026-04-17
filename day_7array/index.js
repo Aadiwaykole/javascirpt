fb74ef682f297221ec505661ee682d4b64ca533e
aditya waykole <adityawaykole494@gmail.com>
2026-04-17 21:21:22 +0530

    object done

---
+++ day_7array/index.js
@@ -0,0 +1,18 @@
+// let marks = [11,22,3,45,56,6]
+
+// console.log(marks)
+// console.log(marks.length)
+
+const arr = [43,32,5,5,65,6]
+
+console.log(arr.splice(1,3));
+
+console.log(arr);
+
+const arr2 = [54,3,3,5,6,76];
+
+let ans = arr2.slice(2,4);
+console.log(ans)
+
+console.log(arr2)
+
