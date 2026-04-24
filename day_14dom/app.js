f089a68a3ccb8e5fe69399f7ecbe4fb1e03c4aac
aditya waykole <adityawaykole494@gmail.com>
2026-04-24 12:39:10 +0530

    file created

---
+++ day_14dom/app.js
@@ -0,0 +1,32 @@
+// const arr = ["pizza","biryani", "dosa", "chai", "samosa"];
+
+const { createElement } = require("react");
+
+// const fragment = document.createDocumentFragment();
+
+// for(let food of arr){
+//     const li = document.createElement("li");
+//     li.textContent = food; 
+//     fragment.append(li);
+
+// }
+
+// const myList = document.getElementById("myList");
+// myList.append(fragment);
+// // STEP 3 - add this now ✅
+// myList.children[2].remove(); 
+
+// //step 4 add this now 
+// const newItem = document.createElement ("li");
+// newItem.textContent = "pav bhaji";
+// myList.prepend(newItem)
+
+const arrey = ["3edi", "chichore", "uri", "12fail ", "dhur"];
+
+const fragment = document.createDocumentFragment();
+
+for(let movies for arrey){
+    const li  = createElement("li");
+    li.textcontent(movies)
+    
+}
