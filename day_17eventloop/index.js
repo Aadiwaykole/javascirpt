3cc5ec79c40113b72472896cc427d5d5534a9889
aditya waykole <adityawaykole494@gmail.com>
2026-04-30 23:34:42 +0530

    eventloop

--- day_17eventloop/index.js
+++ day_17eventloop/index.js
@@ -1,1 +1,12 @@
 console.log("hi")
+let sum = 0
+
+for(let i =0; i <3000000; i++)
+    sum += i; 
+
+console.log(sum);
+console.log("second")
+
+
+//event loop woring with call stack and web API 
+// web API is function call only 
