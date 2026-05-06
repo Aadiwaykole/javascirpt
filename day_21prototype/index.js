e77ac7e457feec1416f7179c2972866082ebdf24
aditya waykole <adityawaykole494@gmail.com>
2026-05-06 23:22:59 +0530

    s

--- day_21prototype/index.js
+++ day_21prototype/index.js
@@ -1,2 +1,30 @@
-//prototype
+// //prototype
 
+// const obj = {
+//     name:"rohit",
+//     age:38,
+//     greet: function (){
+//         console.log("heello jii")
+//     }
+// };
+
+// console.log(obj.name)
+
+// obj.greet()
+
+// obj2  = {
+//     account :30
+// }
+//  obj2.__proto__ = obj
+// console.log(obj2.name);
+
+
+// class 
+
+const obj1 = {
+    name :"rohit",
+    age:30,
+    greet : function (){
+        console.log("hi there ")
+    }
+}
