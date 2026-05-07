a98ee078e056b3018ae8240ebec05e6ac2a78925
aditya waykole <adityawaykole494@gmail.com>
2026-05-07 12:46:34 +0530

    this keeyword

---
+++ day_22thiscall/index.js
@@ -0,0 +1,15 @@
+//this call apply bind 
+
+//this : class object method 
+
+// strict mode vs non strict mode 
+
+
+'use strict '
+
+var a = 10 ; b =20 ; 
+
+console.log(a,b)
+
+function greet(name, name)
+
